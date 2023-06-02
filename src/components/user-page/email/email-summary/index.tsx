ce91009d7c7e30de30d2d8cf14125a0bfcce75e5
LoNGER-2111 <96116356+LoNGER-2111@users.noreply.github.com>
2023-06-02 19:29:19 +0700

    folder navigation completed

--- src/components/user-page/email/email-summary/index.tsx
+++ src/components/user-page/email/email-summary/index.tsx
@@ -1,81 +1,71 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import UserAvatar from "../../../UI-component/user-avatar";
-import { IMessage } from "../../../../interfaces/data.interface";
 import formatTime from "../../../../helpers/format-time.helper";
 import { useLocation, useNavigate } from "react-router-dom";
 
 interface IPropsEmailSummary {
-  messageDetails: IMessage[];
-  folder: string;
+  id: string;
+  name: string;
+  avatarUrl: string;
+  timestamp: string;
+  title: string;
+  content: string;
+  unread: boolean;
 }
 
 const EmailSummary: React.FC<IPropsEmailSummary> = ({
-  messageDetails,
-  folder,
+  id,
+  name,
+  avatarUrl,
+  timestamp,
+  title,
+  content,
+  unread,
 }) => {
-  const [isRead, setIsRead] = useState(false);
   const { pathname } = useLocation();
   const navigate = useNavigate();
-  const folderMessages = useMemo(
-    () =>
-      messageDetails.filter((messageDetail) => messageDetail.folder === folder),
-    [folder, messageDetails]
-  );
+  const [readState, setReadState] = useState<boolean>(unread);
+
+  const isActive = pathname.includes(id);
+
+  const handleClick = () => {
+    navigate(id);
+    setReadState(false);
+  };
 
   return (
-    <>
-      {folderMessages.map(
-        (
-          {
-            id,
-            from: { name, avatarUrl },
-            timestamp,
-            main: { title, content },
-            unread,
-          },
-          index
-        ) => (
-          <div
-            key={index}
-            className={`flex justify-start items-start w-full px-4 py-4 gap-4 border-b border-gray-300 cursor-pointer false ${
-              pathname.includes(id) && "bg-blue-500 text-white"
-            } ${unread && "bg-gray-200"} ${isRead && "bg-white"}`}
-            onClick={() => {
-              navigate(`${id}`);
-              setIsRead(true);
-            }}
+    <div
+      className={`flex justify-start items-start w-full px-4 py-4 gap-4 border-b border-gray-300 cursor-pointer false ${
+        readState && "bg-gray-200"
+      } ${isActive && "bg-blue-500 text-white"}`}
+      onClick={handleClick}
+    >
+      <div className="w-1/6">
+        <UserAvatar avatarUrl={avatarUrl} size="full" />
+      </div>
+      <div className="w-5/6 flex flex-col items-start justify-between">
+        <div
+          className={`flex items-center justify-between w-full text-sm text-gray-500 font-medium ${
+            isActive && "bg-blue-500 text-white"
+          }`}
+        >
+          <p>{name}</p>
+          <p>{formatTime(timestamp, "vi-VN")}</p>
+        </div>
+        <div>
+          <h6 className="text-md font-bold w-full line-clamp-1">{title}</h6>
+        </div>
+        <div className="w-full">
+          <p
+            className={`w-full text-sm line-clamp-3 text-gray-500 ${
+              isActive && "bg-blue-500 text-white"
+            }`}
           >
-            <div className="w-1/6">
-              <UserAvatar avatarUrl={avatarUrl} size="full" />
-            </div>
-            <div className="w-5/6 flex flex-col items-start justify-between">
-              <div
-                className={`flex items-center justify-between w-full text-sm text-gray-500 font-medium ${
-                  pathname.includes(id) && "text-white"
-                }`}
-              >
-                <p>{name}</p>
-                <p>{formatTime(timestamp, "vi-VN")}</p>
-              </div>
-              <div>
-                <h6 className="text-md font-bold w-full line-clamp-1">
-                  {title}
-                </h6>
-              </div>
-              <div className="w-full">
-                <p
-                  className={`w-full text-sm line-clamp-3 text-gray-500 ${
-                    pathname.includes(id) && "text-white"
-                  }`}
-                >
-                  {content}
-                </p>
-              </div>
-            </div>
-          </div>
-        )
-      )}
-    </>
+            {content}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/user-page/email/email-folder/index.tsx
+++ src/pages/user-page/email/email-folder/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import EmailSummary from "../../../../components/user-page/email/email-summary";
 import { IMessage } from "../../../../interfaces/data.interface";
 
@@ -11,12 +11,37 @@ const EmailFolder: React.FC<IPropsEmailFolder> = ({
   messageDetails,
   folder,
 }) => {
+  const folderMessages = useMemo(
+    () =>
+      messageDetails.filter((messageDetail) => messageDetail.folder === folder),
+    [folder, messageDetails]
+  );
+
   return (
     <div
       className="overflow-scroll relative"
       style={{ height: "calc(100% - 3.5rem)" }}
     >
-      <EmailSummary messageDetails={messageDetails} folder={folder} />
+      {folderMessages.map(
+        ({
+          id,
+          from: { name, avatarUrl },
+          timestamp,
+          main: { title, content },
+          unread,
+        }) => (
+          <EmailSummary
+            key={id}
+            id={id}
+            name={name}
+            avatarUrl={avatarUrl}
+            timestamp={timestamp}
+            title={title}
+            content={content}
+            unread={unread}
+          />
+        )
+      )}
     </div>
   );
 };
